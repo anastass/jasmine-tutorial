a5be0adab10e05b0dff85a5385e4b9af82106011
Alexandre Anastassov <a.anastassov@iaea.org>
2015-11-07 23:27:17 +0100

    replace class instance of hello() with a local instances

--- src/hello2.js
+++ src/hello2.js
@@ -1,19 +1,20 @@
 function Hello2() {
 }
 
-Hello2.prototype.hello = new Hello();
-
 Hello2.prototype.world = function() {
-    this.hello.world();
-    this.message = this.hello.message;
+	var hello = new Hello();
+    hello.world();
+    this.message = hello.message;
 }
 
 Hello2.prototype.greeting = function(name) {
-    this.hello.greeting(name);
-    this.message = this.hello.message;
+	var hello = new Hello();
+    hello.greeting(name);
+    this.message = hello.message;
 };
 
 Hello2.prototype.question = function(name) {
-    this.hello.greeting(name);
-    this.message = this.hello.message + " How are you?";
+	var hello = new Hello();
+    hello.greeting(name);
+    this.message = hello.message + " How are you?";
 };
